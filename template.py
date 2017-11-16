0021a937ad5302e87422e7deba32a677bf8eedcd
Luis Henrik John <luishenrikjohn@gmail.com>
2017-11-16 10:49:45 +0100

    Extended template sections by docstrings and meta data

--- template.py
+++ template.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
-# file name        %(filename)
-#Created on %(date)s
-#
-#@author: %(username)s
+"""Brief description to fit on single line.
+
+Elaborate description spanning multiple lines, covering functoinality in more
+detail and explaining module usage.
+"""
+#------------------------------------------------------------------------------#
+#                  MODULE HISTORY                                              #
+#------------------------------------------------------------------------------#
+# Version          1
+# Date             %(date)s
+# Author           %(username)s
+# Note             Original version
 #
 #------------------------------------------------------------------------------#
 #                  SYSTEM IMPORTS                                              #
@@ -15,17 +22,46 @@
 #------------------------------------------------------------------------------#
 
 #------------------------------------------------------------------------------#
+#                  OWN IMPORTS                                                 #
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
+#                  META DATA                                                   #
+#------------------------------------------------------------------------------#
+__author__ = "AB Cooper, DE Fox, and GH Irvine"
+__copyright__ = "Copyright 2017, The ABC Project"
+__credits__ = ["AB Cooper", "DE Fox", "GH Irvine", "JK Lee"]
+__license__ = "GNU"
+__version__ = "1"
+__maintainer__ = "AB Cooper"
+__email__ = "a.b.cooper@abc.edu"
+__status__ = "Development"
+
+#------------------------------------------------------------------------------#
 #                  CONSTANTS                                                   #
 #------------------------------------------------------------------------------#
 
 #------------------------------------------------------------------------------#
 #                  EXPORTED FUNCTIONS                                          #
 #------------------------------------------------------------------------------#
-
+def public_function_example():
+    '''
+    This is a public function example.
+    :return: tmp_bool
+    '''
+    tmp_bool = True
+    return tmp_bool
 #------------------------------------------------------------------------------#
 #                  LOCAL FUNCTIONS                                             #
 #------------------------------------------------------------------------------#
-
+def __private_function_example():
+    '''
+    This is a private function example, which is indicated by the leading under
+    scores.
+    :return: tmp_bool
+    '''
+    tmp_bool = True
+    return tmp_bool
 #------------------------------------------------------------------------------#
 #                  END OF FILE                                                 #
 #------------------------------------------------------------------------------#
