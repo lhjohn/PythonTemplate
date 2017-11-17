b06ecf939b638a903b64f4d53c84e9020d85e61b
Luis Henrik John <luishenrikjohn@gmail.com>
2017-11-17 13:51:17 +0100

    Modified docstring escape characters

--- template.py
+++ template.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Brief description to fit on single line.
+'''Brief description to fit on single line.
 
-Elaborate description spanning multiple lines, covering functoinality in more
-detail and explaining module usage.
-"""
+Elaborate description spanning multiple lines, covering functionality in more
+detail and explaining module usage.'''
 #-----------------------------------------------------------------------------#
 #                  MODULE HISTORY                                             #
 #-----------------------------------------------------------------------------#
@@ -28,27 +27,27 @@ detail and explaining module usage.
 #-----------------------------------------------------------------------------#
 #                  META DATA                                                  #
 #-----------------------------------------------------------------------------#
-__author__ = "AB Cooper, DE Fox, and GH Irvine"
-__copyright__ = "Copyright 2017, The ABC Project"
-__credits__ = ["AB Cooper", "DE Fox", "GH Irvine", "JK Lee"]
-__license__ = "GNU"
-__version__ = "1"
-__maintainer__ = "AB Cooper"
-__email__ = "a.b.cooper@abc.edu"
-__status__ = "Development"
+__author__ = 'AB Cooper, DE Fox, and GH Irvine'
+__copyright__ = 'Copyright 2017, The ABC Project'
+__credits__ = ['AB Cooper', 'DE Fox', 'GH Irvine', 'JK Lee']
+__license__ = 'GNU'
+__version__ = '1'
+__maintainer__ = 'AB Cooper'
+__email__ = 'a.b.cooper@abc.edu'
+__status__ = 'Development'
 
 #-----------------------------------------------------------------------------#
 #                  CONSTANTS                                                  #
 #-----------------------------------------------------------------------------#
+# ratio of a circle's circumference to its diameter
+PI = 3.141592
 
 #-----------------------------------------------------------------------------#
 #                  EXPORTED FUNCTIONS                                         #
 #-----------------------------------------------------------------------------#
 def public_function_example():
-    '''
-    This is a public function example.
-    :return: tmp_bool
-    '''
+    '''This is a public function example.
+    :return: tmp_bool'''
     tmp_bool = True
     return tmp_bool
 
@@ -56,11 +55,9 @@ def public_function_example():
 #                  LOCAL FUNCTIONS                                            #
 #-----------------------------------------------------------------------------#
 def __private_function_example():
-    '''
-    This is a private function example, which is indicated by the leading under
-    scores.
-    :return: tmp_bool
-    '''
+    '''This is a private function example, which is indicated by the leading 
+    under scores.
+    :return: tmp_bool'''
     tmp_bool = True
     return tmp_bool
 
